00d228187d060b131fc042036ffabdcfb50e2453
pianisthub <rangarao364@gmail.com>
2025-11-25 15:31:06 +0530

    frontend lambda reactjs commit handler module fix

--- bookstore-frontend/handler.js
+++ bookstore-frontend/handler.js
@@ -3,18 +3,14 @@ const serverless = require('serverless-http');
 const path = require('path');
 const app = express();
 
-
-console.log("Lambda starting...");
-
 const buildPath = path.join(__dirname, 'build');
-
 
 app.use(express.static(buildPath));
 
-
 app.get('/*', (req, res) => {
-  const indexPath = path.join(buildPath, 'index.html');
-  res.sendFile(indexPath);
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
-module.exports.handler = serverless(app);
+ 
+const handler = serverless(app);
+module.exports.hello = handler;
